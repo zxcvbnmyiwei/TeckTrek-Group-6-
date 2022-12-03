05a130192f2cfb2ebf50a610f1a021c57dd3826c
Rouxuanpipi <imrouxuan@gmail.com>
2022-12-03 14:07:08 +0800

    Update Transaction Schema

--- backend/models/Schema.js
+++ backend/models/Schema.js
@@ -13,16 +13,4 @@ const userSchema = mongoose.Schema({
 })
 const User = mongoose.model("User", userSchema)
 
-// ScheduledTransaction
-const transactionSchema = mongoose.Schema({
-    TransactionID: Number,
-    AccountID: Number,
-    ReceivingAccountID: Number,
-    Date: Date,
-    TransactionAmount: Number,
-    Comment: String,
-})
-const Transaction = mongoose.model("Transaction", transactionSchema)
-
-
-module.exports = {User, Transaction}
+module.exports = {User}
---
+++ backend/models/transaction.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema({
+    TransactionID: {type: Number, required: true},
+    AccountID: {type: Number, required: true},
+    ReceivingAccountID: {type: Number, required: true},
+    Date: {type: Date, required: true},
+    TransactionAmount: {type: Number, required: true},
+    Comment: {type: String},
+}, {
+    timestamps: true,
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema)
+
+module.exports = Transaction
+
