b4ec1351dfd62abbffd0ce2102ebd2e8624db508
Rouxuanpipi <imrouxuan@gmail.com>
2022-12-03 16:02:25 +0800

    Update transactionController.js

--- backend/controller/transactionController.js
+++ backend/controller/transactionController.js
@@ -5,8 +5,9 @@ const addTransaction = async (req,res) => {
     try {
         const numID = await Transaction.find().count() + 1;
         req.body.TransactionID = numID
-        const {TransactionID,ReceivingAccountID,Date,TransactionAmount,Comment} = req.body;
-        const transaction = await new Transaction({TransactionID,ReceivingAccountID,Date,TransactionAmount,Comment}).save();
+        // Need Account ID... 
+        const {TransactionID,AccountID,ReceivingAccountID,Date,TransactionAmount,Comment} = req.body;
+        const transaction = await new Transaction({TransactionID, AccountID ,ReceivingAccountID,Date,TransactionAmount,Comment}).save();
 
         return res.status(201).json({success: true, transaction});
     
